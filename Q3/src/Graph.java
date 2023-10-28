7d09a77fe3b26f62e791e66f6322760aaa7737f9
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2023-10-28 09:21:51 +0700

    Graph

--- Q3/src/Graph.java
+++ Q3/src/Graph.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.util.*;
 //-------------------------------------------------------------------------------
 
-public class Graph{
+public class Graph {
 
     int[][] a;
     int n;
@@ -95,6 +95,37 @@ public class Graph{
         }
     }
 
+    void breadth(boolean[] en, int i, ArrayList<Integer> count, RandomAccessFile f) throws Exception {
+        Queue q = new Queue();
+        int r, j;
+        q.enqueue(i);
+        en[i] = true;
+        while (!q.isEmpty()) {
+            r = q.dequeue();
+            count.add(r);
+            for (j = 0; j < n; j++) {
+                if (!en[j] && a[r][j] > 0) {
+                    q.enqueue(j);
+                    en[j] = true;
+                }
+            }
+        }
+    }
+
+    void breadth(int k, ArrayList<Integer> count, RandomAccessFile f) throws Exception {
+        boolean[] en = new boolean[20];
+        int i;
+        for (i = 0; i < n; i++) {
+            en[i] = false;
+        }
+        breadth(en, k, count, f);
+        for (i = 0; i < n; i++) {
+            if (!en[i]) {
+                breadth(en, i, count, f);
+            }
+        }
+    }
+
     void depth(boolean[] visited, int k, RandomAccessFile f) throws Exception {
         fvisit(k, f);
         visited[k] = true;
@@ -119,22 +150,224 @@ public class Graph{
         }
     }
 
+    void depth(boolean[] visited, int k, ArrayList<Integer> count, RandomAccessFile f) throws Exception {
+        count.add(k);
+        visited[k] = true;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && a[k][i] > 0) {
+                depth(visited, i, count, f);
+            }
+        }
+    }
+
+    void depth(int k, ArrayList<Integer> count, RandomAccessFile f) throws Exception {
+        boolean[] visited = new boolean[20];
+        int i;
+        for (i = 0; i < n; i++) {
+            visited[i] = false;
+        }
+        depth(visited, k, count, f);
+        for (i = 0; i < n; i++) {
+            if (!visited[i]) {
+                depth(visited, i, count, f);
+            }
+        }
+    }
+
     int deg(int i) {
         int s, j;
         s = 0;
         for (j = 0; j < n; j++) {
-            if (a[i][j] > 0 && a[i][j] < 999) {
-                s += a[i][j];
-            }
+            s += a[i][j];
         }
-//    s += a[i][i];
+        s += a[i][i];
         return (s);
     }
-  
+
 //===========================================================================
 //(2)===YOU CAN EDIT OR EVEN ADD NEW FUNCTIONS IN THE FOLLOWING PART========
 //===========================================================================
- 
+    void depth2(boolean[] visited, int k, RandomAccessFile f) throws Exception {
+        fvisit(k, f);
+        f.writeBytes("(" + deg(k) + ")");
+        visited[k] = true;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && a[k][i] > 0) {
+                depth2(visited, i, f);
+            }
+        }
+    }
+
+    void depth2(int k, RandomAccessFile f) throws Exception {
+        boolean[] visited = new boolean[20];
+        int i;
+        for (i = 0; i < n; i++) {
+            visited[i] = false;
+        }
+        depth2(visited, k, f);
+        for (i = 0; i < n; i++) {
+            if (!visited[i]) {
+                depth2(visited, i, f);
+            }
+        }
+    }
+
+    void breadth2(boolean[] en, int i, RandomAccessFile f) throws Exception {
+        Queue q = new Queue();
+        int r, j;
+        q.enqueue(i);
+        en[i] = true;
+        f.writeBytes("\n");
+        while (!q.isEmpty()) {
+            r = q.dequeue();
+            f.writeBytes(deg(r) + " ");
+            for (j = 0; j < n; j++) {
+                if (!en[j] && a[r][j] > 0) {
+                    q.enqueue(j);
+                    en[j] = true;
+                }
+            }
+        }
+    }
+
+    void breadth2(int k, RandomAccessFile f) throws Exception {
+        boolean[] en = new boolean[20];
+        int i;
+        for (i = 0; i < n; i++) {
+            en[i] = false;
+        }
+        breadth2(en, k, f);
+        for (i = 0; i < n; i++) {
+            if (!en[i]) {
+                breadth2(en, i, f);
+            }
+        }
+    }
+
+    void dijkstra(int from, int to, RandomAccessFile f) throws Exception {
+        boolean[] visited = new boolean[n];
+        int[] distance = new int[n];
+        int[] previous = new int[n];
+        int INF = 99;
+
+        for (int i = 0; i < n; i++) {
+            visited[i] = false;
+            distance[i] = a[from][i];
+            previous[i] = from;
+        }
+
+        visited[from] = true;
+
+        while (true) {
+            int minDistance = INF;
+            int k = -1;
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == true) {
+                    continue;
+                }
+                if (distance[i] < minDistance) {
+                    minDistance = distance[i];
+                    k = i;
+                }
+            }
+            if (k == -1) {
+                return;
+            }
+            visited[k] = true;
+            if (k == to) {
+                break;
+            }
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == true) {
+                    continue;
+                }
+                if (distance[i] > distance[k] + a[k][i]) {
+                    distance[i] = distance[k] + a[k][i];
+                    previous[i] = k;
+                }
+            }
+        }
+        ArrayList<Integer> points = new ArrayList<>();
+        points.add(points.size(), to);
+        while (from != to) {
+            to = previous[to];
+            points.add(points.size(), to);
+        }
+        for (int i = points.size() - 1; i >= 0; i--) {
+//            System.out.print(v[points.get(i)] + " ");
+            f.writeBytes(v[points.get(i)] + " ");
+        }
+//        System.out.println("");
+        f.writeBytes("\n");
+        for (int i = points.size() - 1; i >= 0; i--) {
+//            System.out.print(distance[points.get(i)] + " ");
+            f.writeBytes(distance[points.get(i)] + " ");
+        }
+    }
+
+    void dijkstra(int from, int to, ArrayList<Character> ver, ArrayList<Integer> dis) throws Exception {
+        boolean[] visited = new boolean[n];
+        int[] distance = new int[n];
+        int[] previous = new int[n];
+        int INF = 999;
+
+        for (int i = 0; i < n; i++) {
+            visited[i] = false;
+            distance[i] = a[from][i];
+            previous[i] = from;
+        }
+
+        visited[from] = true;
+
+        while (true) {
+            int minDistance = INF;
+            int k = -1;
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == true) {
+                    continue;
+                }
+                if (distance[i] < minDistance) {
+                    minDistance = distance[i];
+                    k = i;
+                }
+            }
+            if (k == -1) {
+                return;
+            }
+            visited[k] = true;
+            if (k == to) {
+                break;
+            }
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == true) {
+                    continue;
+                }
+                if (distance[i] > distance[k] + a[k][i]) {
+                    distance[i] = distance[k] + a[k][i];
+                    previous[i] = k;
+                }
+            }
+        }
+        ArrayList<Integer> points = new ArrayList<>();
+        points.add(points.size(), to);
+        while (from != to) {
+            to = previous[to];
+            points.add(points.size(), to);
+        }
+        for (int i = points.size() - 1; i >= 0; i--) {
+//            System.out.print(v[points.get(i)] + " ");
+//            f.writeBytes(v[points.get(i)] + " ");
+            ver.add(v[points.get(i)]);
+        }
+//        System.out.println("");
+//        f.writeBytes("\n");
+        for (int i = points.size() - 1; i >= 0; i--) {
+//            System.out.print(distance[points.get(i)] + " ");
+//            f.writeBytes(distance[points.get(i)] + " ");
+            dis.add(distance[points.get(i)]);
+        }
+    }
+
     void f1(int lineNum) throws Exception {
         loadData(lineNum);
         String fname = "f1.txt";
@@ -146,9 +379,9 @@ public class Graph{
         depth(1, f);
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
-   
- 
+
         //-------------------------------------------------------------------------------------
+        depth2(1, f);
         f.writeBytes("\r\n");
         f.close();
     }
@@ -166,9 +399,10 @@ public class Graph{
         /*You must keep statements pre-given in this function.
        Your task is to insert statements here, just after this comment,
        to complete the question in the exam paper.*/
-      
- 
+
         //-------------------------------------------------------------------------------------
+        breadth(0, f);
+        breadth2(0, f);
         f.writeBytes("\r\n");
         f.close();
     }
@@ -193,7 +427,6 @@ while(S is not empty)
      */
     int count = 0;
 
- 
     void f3(int lineNum) throws Exception {
         loadData(lineNum);
         String fname = "f1.txt";
@@ -206,13 +439,16 @@ while(S is not empty)
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
 
-        
         //-------------------------------------------------------------------------------------
+        ArrayList<Integer> count = new ArrayList<>();
+        depth(1, count, f);
+        for (int i = 2; i <= 5; i++) {
+            fvisit(count.get(i), f);
+        }
         f.writeBytes("\r\n");
         f.close();
     }
 
- 
     void f4(int lineNum) throws Exception {
         loadData(lineNum);
         String fname = "f1.txt";
@@ -225,13 +461,16 @@ while(S is not empty)
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
 
-        
         //-------------------------------------------------------------------------------------
+        ArrayList<Integer> count = new ArrayList<>();
+        breadth(1, count, f);
+        for (int i = 1; i <= 3; i++) {
+            fvisit(count.get(i), f);
+        }
         f.writeBytes("\r\n");
         f.close();
     }
 
- 
     void f5(int lineNum) throws Exception {
         loadData(lineNum);
         String fname = "f1.txt";
@@ -243,12 +482,11 @@ while(S is not empty)
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
 
-        
         //-------------------------------------------------------------------------------------
+        dijkstra(1, 4, f);
         f.writeBytes("\r\n");
         f.close();
     }
-
 
     void f6(int lineNum) throws Exception {
         loadData(lineNum);
@@ -260,14 +498,30 @@ while(S is not empty)
         RandomAccessFile f = new RandomAccessFile(fname, "rw");
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
- 
         //-------------------------------------------------------------------------------------
+
+        ArrayList<Character> v1 = new ArrayList<>();
+        ArrayList<Integer> dis1 = new ArrayList<>();
+        dijkstra(1, 4, v1, dis1);
+        f.writeBytes(v1.get(0) + " " + v1.get(v1.size() - 1) + "->" + dis1.get(dis1.size() - 1) + "\n");
+
+        ArrayList<Character> v2 = new ArrayList<>();
+        ArrayList<Integer> dis2 = new ArrayList<>();
+        dijkstra(0, 5, v2, dis2);
+        if (v2.size() >= 4) {
+            for (int i = v2.size() - 4; i < v2.size(); i++) {
+                f.writeBytes(v2.get(i) + "-" + dis2.get(i) + " ");
+            }
+        } else {
+            for (int i = 0; i < v2.size(); i++) {
+                f.writeBytes(v2.get(i) + "-" + dis2.get(i) + " ");
+            }
+        }
         f.writeBytes("\r\n");
         f.close();
     }
 
-
-   void f7(int lineNum) throws Exception {
+    void f7(int lineNum) throws Exception {
         loadData(lineNum);
         String fname = "f1.txt";
         File g123 = new File(fname);
@@ -277,13 +531,35 @@ while(S is not empty)
         RandomAccessFile f = new RandomAccessFile(fname, "rw");
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
-    
+
         //-------------------------------------------------------------------------------------
+        ArrayList<Character> v1 = new ArrayList<>();
+        ArrayList<Integer> dis1 = new ArrayList<>();
+        dijkstra(1, 4, v1, dis1);
+
+        for (int i = 0; i < v1.size(); i++) {
+            f.writeBytes(v1.get(i) + " ");
+        }
+        f.writeBytes("\n" + dis1.get(dis1.size() - 1) + "\n");
+
+        ArrayList<Character> v2 = new ArrayList<>();
+        ArrayList<Integer> dis2 = new ArrayList<>();
+        dijkstra(1, 6, v2, dis2);
+
+//        for (int i = 0; i <= 3; i++) {
+//            f.writeBytes(v2.get(i) + " ");
+////            f.writeBytes(v2.get(i) + "-" + dis2.get(i) + " ");
+//        }
+        for (Character temp : v2) {
+            System.out.println(temp + " ");
+        }
+        for (int temp : dis2) {
+            System.out.println(temp + " ");
+        }
         f.writeBytes("\r\n");
         f.close();
     }
 
- 
     void f8(int lineNum) throws Exception {
         loadData(lineNum);
         String fname = "f1.txt";
@@ -294,12 +570,46 @@ while(S is not empty)
         RandomAccessFile f = new RandomAccessFile(fname, "rw");
         f.writeBytes("\r\n");
         //-------------------------------------------------------------------------------------
-  
-        
+
         //-------------------------------------------------------------------------------------
-         
+        eulerCycle(4, f);
         f.writeBytes("\r\n");
         f.close();
     }
- 
+
+    void eulerCycle(int k, RandomAccessFile f) throws Exception {
+//        if (!hasEulerCycle()) {
+//            System.out.println("Condicitons for Euler's cycle are not satisfied");
+//            return;
+//        }
+        int[] eu = new int[100];
+        int m = 0;
+        int i, j, r;
+        Stack s = new Stack();
+        s.push(k);
+        while (!s.isEmpty()) {
+            r = s.top();
+            for (i = 0; i < n; i++) {
+                if (a[r][i] > 0) {
+                    break;
+                }
+            }
+            if (i == n) { // r isolated
+                s.pop();
+                eu[m++] = r;
+            } else {
+                s.push(i);
+                a[r][i]--;
+                a[i][r]--;
+            }
+        }
+//        System.out.println("Euler's cycle is:");
+//        System.out.print(v[eu[0]]);
+        f.writeBytes(v[eu[0]] + "");
+        for (i = 1; i < m; i++) {
+//            System.out.print(" " + v[eu[i]]);
+            f.writeBytes(" " + v[eu[i]]);
+        }
+        System.out.println();
+    }
 }
